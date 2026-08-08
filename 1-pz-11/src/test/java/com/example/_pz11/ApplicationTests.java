486b0f125dbf863e6e98d0adb17373e1795af05f
sergijluk <Sergii.Lyuk@externals.adidas.com>
2026-08-08 06:18:22 +0300

    first commit

---
+++ 1-pz-11/src/test/java/com/example/_pz11/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example._pz11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
